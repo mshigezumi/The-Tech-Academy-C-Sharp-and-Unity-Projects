beca988a59a929cf4514523a815a5781fa798ea9
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-16 11:16:56 -0700

    Update Assignments

---
+++ Assignments/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = age > 15 && DUI == false && tickets <= 3;
+            Console.WriteLine(qualified);
+            Console.Read();
+        }
+    }
+}
