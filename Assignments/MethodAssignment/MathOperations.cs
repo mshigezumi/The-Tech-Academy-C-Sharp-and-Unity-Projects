90aa2b8abaa60777ecfab3ecb3cfb74f96c90bc9
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-17 10:27:10 -0700

    Update Assignments

---
+++ Assignments/MethodAssignment/MathOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class MathOperations
+    {
+        public int Add(int x, int y = 0)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Assignments/MethodAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a second number (optional)");
+            string input = Console.ReadLine();
+            MathOperations math = new MathOperations();
+            if (input != "")
+            {
+                int number2 = Convert.ToInt32(input);
+                Console.WriteLine(math.Add(number, number2));
+            }
+            else
+            {
+                Console.WriteLine(math.Add(number));
+            }
+            Console.Read();
+        }
+    }
+}
