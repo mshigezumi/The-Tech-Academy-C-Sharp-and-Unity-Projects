390218d5a1a01383d1e3ea0730ad3dcee0a73cbe
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-17 09:13:39 -0700

    Update Assignments

---
+++ Assignments/ConsoleAppAssignment2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] PhoneticAlphabet = { "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett" };
+            Console.WriteLine("Input text to be added to each string");
+            string add = Console.ReadLine();
+            int i = 0;
+            foreach (string letter in PhoneticAlphabet)
+            {
+                PhoneticAlphabet[i] = letter + add;
+                i++;
+            }
+            foreach (string letter in PhoneticAlphabet)
+            {
+                Console.WriteLine(letter);
+            }
+            
+        }
+    }
+}
