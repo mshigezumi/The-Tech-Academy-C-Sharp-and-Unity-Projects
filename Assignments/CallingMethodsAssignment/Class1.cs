1311d24d19253d1d78f26df382d1fb3357cbbd3e
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-17 10:19:08 -0700

    Update Assignments

---
+++ Assignments/CallingMethodsAssignment/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class MathOperations
+    {
+        public int Add(int x)
+        {
+            return 10 + x;
+        }
+
+        public int Subtract(int x)
+        {
+            return 10 - x;
+        }
+
+        public int Multiply(int x)
+        {
+            return 10 * x;
+        }
+    }
+}
---
+++ Assignments/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            MathOperations math = new MathOperations();
+            Console.WriteLine(math.Add(number));
+            Console.WriteLine(math.Subtract(number));
+            Console.WriteLine(math.Multiply(number));
+            Console.Read();
+        }
+    }
+}
