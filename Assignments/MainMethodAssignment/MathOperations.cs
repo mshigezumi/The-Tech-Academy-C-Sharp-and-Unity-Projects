d3efae8d7af65a50987fd9fdc48cbfac187b17a5
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-17 10:24:04 -0700

    Update Assignments

---
+++ Assignments/MainMethodAssignment/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MathOperations
+    {
+        public int Add(int x)
+        {
+            return 10 + x;
+        }
+
+        public int Add(decimal x)
+        {
+            return Convert.ToInt32(20 + x);
+        }
+
+        public int Add(string x)
+        {
+            return 30 + Convert.ToInt32(x);
+        }
+    }
+}
---
+++ Assignments/MainMethodAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = 5;
+            decimal number2 = (decimal)5.5;
+            string number3 = "5";
+            MathOperations math = new MathOperations();
+            Console.WriteLine(math.Add(number));
+            Console.WriteLine(math.Add(number2));
+            Console.WriteLine(math.Add(number3));
+            Console.Read();
+        }
+    }
+}
