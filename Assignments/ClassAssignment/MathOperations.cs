13824b263234644c1d0dffec057fe8f506956a35
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-18 09:17:26 -0700

    Update Assignments

---
+++ Assignments/ClassAssignment/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    static class MathOperations
+    {
+        public static void Divide(int x)
+        {
+            x = x / 2;
+            Console.WriteLine(x);
+        }
+
+        public static void Set(out int x, out int y)
+        {
+            x = 10;
+            y = 2;
+        }
+
+        public static void Divide(int x, int y)
+        {
+            x = x / y;
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Assignments/ClassAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            MathOperations.Divide(number);
+            Console.WriteLine("--");
+            MathOperations.Set(out int x, out int y); //sets the variables to x = 10 and y = 2
+            MathOperations.Divide(x, y);
+            Console.Read();
+        }
+    }
+}
