a1959f943595b9b0ced271bb3b3881c2105bffab
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-18 12:11:40 -0700

    Update Assignments

---
+++ Assignments/PolymorphismAssignment/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Time to quit");
+        }
+    }
+}
---
+++ Assignments/PolymorphismAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quit = new Employee();
+            quit.Quit();
+            Console.Read();
+        }
+    }
+}
