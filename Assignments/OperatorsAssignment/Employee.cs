a8493207f253e4b5648e3f54362df56525ae2dd1
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-18 12:21:03 -0700

    Update Assignments

---
+++ Assignments/OperatorsAssignment/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Employee : Person
+    {
+        public int ID;
+
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Time to quit");
+        }
+
+        public static bool operator ==(Employee alfa, Employee bravo)
+        {
+            if (alfa.ID == bravo.ID)
+                return true;
+            else
+                return false;
+
+        }
+
+        public static bool operator !=(Employee alfa, Employee bravo)
+        {
+            if (alfa.ID != bravo.ID)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Assignments/OperatorsAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee alfa = new Employee();
+            Employee bravo = new Employee();
+
+            alfa.ID = 10;
+            bravo.ID = 10;
+            if (alfa == bravo)
+            {
+                Console.WriteLine(alfa.ID + " is the same as " + bravo.ID);
+            }
+            else
+            {
+                Console.WriteLine(alfa.ID + " is not the same as " + bravo.ID);
+            }
+            Console.Read();
+        }
+    }
+}
