5db555ef661918279ba60877103aa75f3e81aad0
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-18 11:13:59 -0700

    Update Assignments

---
+++ Assignments/MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void SayName()
+        {
+            Console.Write("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
