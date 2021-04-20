f62f48367c74c1ca408a78842d75f32c6439d1ec
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-20 08:43:54 -0700

    Update Assignments

---
+++ Assignments/ParametersAssignment/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Employee<gen> : Person
+    {
+        public int ID;
+        public List<gen> things;
+
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Time to quit");
+        }
+
+        public static bool operator ==(Employee<gen> alfa, Employee<gen> bravo)
+        {
+            if (alfa.ID == bravo.ID)
+                return true;
+            else
+                return false;
+
+        }
+
+        public static bool operator !=(Employee<gen> alfa, Employee<gen> bravo)
+        {
+            if (alfa.ID != bravo.ID)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Assignments/ParametersAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> alfa = new Employee<string>();
+            alfa.things = new List<string>() { "Alfa", "Bravo", "Charlie", "Delta", "Echo" };
+            Employee<int> bravo = new Employee<int>();
+            bravo.things = new List<int>() { 1, 2, 3, 4, 5 };
+
+            foreach (string item in alfa.things)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (int item in bravo.things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+    }
+}
