df561002d600a71b5575770a8bae232ed4f9f834
mshigezumi <71560004+mshigezumi@users.noreply.github.com>
2021-04-20 10:34:22 -0700

    Update Assignments

---
+++ Assignments/DateTimeAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime();
+            dt = DateTime.Now;
+            Console.WriteLine(dt);
+            Console.WriteLine("Enter a number of hours: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            TimeSpan ts = new TimeSpan(0, input, 0, 0);
+            Console.WriteLine(dt + ts);
+            Console.Read();
+        }
+    }
+}
